94fb1cb6218b8614bb28ca35675696d186fc25bd
sumanni <suman.niraula00@gmail.com>
2026-01-13 18:36:54 +0000

    HERO SECTION ADDED

--- components/hero-section-demo-1.tsx
+++ components/hero-section-demo-1.tsx
@@ -1,14 +1,14 @@
 "use client";
 
+
 import { motion } from "motion/react";
-import { Bento } from "./_component/Bento";
 
 export default function HeroSectionOne() {
   return (
-    <div className="relative  my-10 flex flex-col items-center justify-center">
+    <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center">
       <Navbar />
       <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
-        <div className="absolute top-0 h-40 w-px bg-linear-to-b from-transparent via-blue-500 to-transparent" />
+        <div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
       </div>
       <div className="absolute inset-y-0 right-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
         <div className="absolute h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
@@ -18,7 +18,7 @@ export default function HeroSectionOne() {
       </div>
       <div className="px-4 py-10 md:py-20">
         <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
-          {"Consult doctors by voice,healthcare made easy."
+          {"Launch your website in hours, not days"
             .split(" ")
             .map((word, index) => (
               <motion.span
@@ -49,8 +49,9 @@ export default function HeroSectionOne() {
           }}
           className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
         >
-          Talk to a doctor instantly using just your voice. Get medical guidance
-          without appointments or waiting rooms.
+          With AI, you can launch your website in hours, not days. Try our best
+          in class, state of the art, cutting edge AI tools to get your website
+          up.
         </motion.p>
         <motion.div
           initial={{
@@ -91,14 +92,13 @@ export default function HeroSectionOne() {
             <img
               src="https://assets.aceternity.com/pro/aceternity-landing.webp"
               alt="Landing page preview"
-              className=" h-auto w-full object-cover"
+              className="aspect-[16/9] h-auto w-full object-cover"
               height={1000}
               width={1000}
             />
           </div>
         </motion.div>
       </div>
-      <Bento />
     </div>
   );
 }
